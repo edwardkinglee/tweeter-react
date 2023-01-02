447431a9f4efc58df31319318a983059010334a5
edwardkinglee <edward.lee3@telus.com>
2023-01-01 19:49:00 -0500

    Add css file and link in head

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -9,8 +9,10 @@ function App() {
     <div className="App">
       <Navigation />
       <Profile />
-      <TweetList />
-      <TweetForm />
+      <main className="container">
+        <TweetList />
+        <TweetForm />
+      </main>
     </div>
   );
 }
