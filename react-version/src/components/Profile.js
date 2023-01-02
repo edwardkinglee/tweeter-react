8af77d3f634c6a71d62882b1c364450bc3250637
edwardkinglee <edward.lee3@telus.com>
2023-01-01 20:11:35 -0500

    Change to variables and add tweet.js

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,14 +1,18 @@
 import React from 'react'; //optional
 
   function Profile(){
+    const firstName = "Amy"
+    const lastName = "Mansell"
+    const avatar = "/profile-hex.png"
+
     return (
       <aside>
     <div class="profile">
-      <img class="profile__image" src="./profile-hex.png" />
+      <img class="profile__image" src={avatar} />
     </div>
     <br/>
     <div class="profile__name">
-      <h2><span class="profile--bold">Amy</span> Mansell</h2>
+      <h2><span class="profile--bold">{firstName}</span> {lastName}</h2>
     </div>
   </aside>
     );
